0cc5ab0a9e0a8a2de9af9fd5b1e4f3ac0139f52c
trunks07 <carlog@mkthemedattractions.com.ph>
2026-03-31 14:38:51 +0800

    Minor tweaks

--- app/models/Attendance.py
+++ app/models/Attendance.py
@@ -154,7 +154,7 @@ class AttendanceModel:
     async def create(
         self, attendance_data: AttendanceCreate, session: Optional[AgnosticClientSession] = None
     ) -> Dict[str, Any]:
-        item_dict = attendance_data.model_dump()
+        # item_dict = attendance_data.model_dump()
         item_dict: Dict[str, Any] = attendance_data.model_dump()
         item_dict.setdefault("created_at", datetime.now())
         item_dict.setdefault("updated_at", datetime.now())
